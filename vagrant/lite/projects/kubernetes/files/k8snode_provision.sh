5a6a8fdbafa5f0e8f2188d594edca08b5c9ad974
Justin Cook <jhcook@gmail.com>
2016-06-02 12:34:02 +0100

    Update the comments

--- vagrant/lite/projects/kubernetes/files/k8snode_provision.sh
+++ vagrant/lite/projects/kubernetes/files/k8snode_provision.sh
@@ -24,7 +24,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Provision a Kubernetes node on CentOS Atomic
+# Provision a Kubernetes node on RHEL7 or RHEL Atomic
 # https://access.redhat.com/documentation/
 #+en/red-hat-enterprise-linux-atomic-host/version-7/getting-started-guide/
 
